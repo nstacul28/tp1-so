bcafea42fe723b62073fbb109e66e243d50ecfc7
Ballest-fran <88737673+Ballest-fran@users.noreply.github.com>
2021-09-14 16:15:07 -0300

    primera subida

---
+++ pruebas.py
@@ -0,0 +1,14 @@
+"""a={
+  "a":2
+}
+m=list()
+for i in range(4):
+  a["a"]=i
+  print(a["a"])
+  m.append(a)
+
+print(m)"""
+for j in range(15):
+  for i in range(10):
+    print("a", end=" ")
+  print()
