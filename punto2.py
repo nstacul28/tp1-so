10b6580c89a27f55c980d4048cbe76ebce462575
Ballest-fran <88737673+Ballest-fran@users.noreply.github.com>
2021-09-18 19:58:02 -0300

    create punto2.py

---
+++ punto2.py
@@ -0,0 +1,9 @@
+"""
+1.2	Modificar la propuesta anterior con la posibilidad de cargar N procesos y utilizar particiones fijas.
+ El programa debe permitir ingresar nuevos procesos, mientras haya memoria libre para asignar, por cada 
+ proceso se debe ingresar o leer de un archivo el Id de proceso, tamaño del proceso, tiempo de arribo y 
+ tiempo de irrupción. La tabla de particiones impresa deberá contener (Id de partición, dirección de comienzo 
+ de partición, tamaño de la partición, id de proceso asignado a la partición, fragmentación interna). 
+ Mostrar la información anterior por cada proceso ingresado en la tabla de particiones.
+"""
+
